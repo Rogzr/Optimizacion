d66e694e7dd85e183d3e223fa9aa33ce22ce95fd
rodri <rodri@X13>
2023-10-03 17:15:11 -0600

    Add project files.

---
+++ Act1/Form1.cs
@@ -0,0 +1,10 @@
+namespace Act1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Act1/Models/Vehiculo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act1.Models
+{
+    public class Vehiculo
+    {
+        // ID del vehiculo
+        public int ID { get; set; }
+        public double Capacidad { get; set; }
+        public long LatitudOrigen { get; set;}
+        public long LongitudOrigen { get; set; }
+        public DateTime FechaDisp { get; set; }
+
+        public Vehiculo() { }
+
+        public Vehiculo(int iD, double capacidad, long latitudOrigen, long longitudOrigen, DateTime fechaDisp)
+        {
+            ID = iD;
+            Capacidad = capacidad;
+            LatitudOrigen = latitudOrigen;
+            LongitudOrigen = longitudOrigen;
+            FechaDisp = fechaDisp;
+        }
+    }
+}
